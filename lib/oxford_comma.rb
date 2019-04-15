5c6006394d9b04bd19d9f429da5b0c2bdfbd0ac3
Garth Lilly <garff247@gmail.com>
2019-04-15 03:28:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  x = array.length
+  string = String.new
+  if x < 3
+  string = array.join(" and ")
+  else
+  array.last("and")
+  string = array.join(", ")
+end
+string
 end
